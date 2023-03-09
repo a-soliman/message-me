d18ccea15ef5682cd3e0f41a0e4298c05d47dcd5
Ahmed Soliman <ahmed@famous.co>
2023-03-09 17:21:16 -0600

    added login functionality

--- app/controllers/chatroom_controller.rb
+++ app/controllers/chatroom_controller.rb
@@ -1,5 +1,6 @@
 class ChatroomController < ApplicationController
   def index
+    redirect_to login_path unless logged_in?
     @messages = Message.all
   end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,27 @@
 class SessionsController < ApplicationController
   def new
+    redirect_to root_path if logged_in?
+  end
+
+  def create
+    session_params = params[:session]
+    username_param = session_params[:username]
+    user = User.find_by(username: username_param)
+
+    if user 
+      password_param = session_params[:password]
+      if user.authenticate(password_param)
+        # authenticated
+        session[:user_id] = user.id
+        flash[:notice] = "Welcome back #{user.username}!"
+        redirect_to root_path
+      else
+        flash.now[:alert] = "Invalid password"
+        render 'new', status: :unauthorized
+      end
+    else
+      flash.now[:alert] = "Invalid login details"
+      render 'new', status: :not_found
+    end
   end
 end
